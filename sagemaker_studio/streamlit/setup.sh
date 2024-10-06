2bf9e58810df3dc3d1960d6f9f21ab7289865417
Jason Stollings <jstollings@yahoo.com>
2024-10-06 13:51:51 -0500

    Add files via upload

--- sagemaker_studio/streamlit/setup.sh
+++ sagemaker_studio/streamlit/setup.sh
@@ -3,7 +3,16 @@
 
 # Setup script to prepare to run streamlit web user interface in SageMaker Studio
 
+# Create and activate Conda environment with Python 3.11
+conda create -y --name my_python_311 python=3.11 pip
+conda activate my_python_311
+
+# Install dependencies
 pip install --no-cache-dir -r requirements.txt
 sudo yum install -y iproute
 sudo yum install -y jq
 sudo yum install -y lsof
+
+# Make the run and cleanup scripts executable
+chmod +x ./run.sh
+chmod +x ./cleanup.sh
